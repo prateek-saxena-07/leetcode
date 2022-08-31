41ce2f82244b6d5bdd4c274e15b6d0332ed8b12c
prateek-saxena-07 <86514419+prateek-saxena-07@users.noreply.github.com>
2022-08-31 19:10:59 +0530

    Time: 4 ms (82.19%), Space: 9.5 MB (94.70%) - LeetHub

---
+++ 35-search-insert-position/35-search-insert-position.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+ int searchInsert(vector<int>& nums, int target) {
+        int ans=binary(nums,target,0,nums.size()-1);
+        return ans;
+        
+    }
+    
+    int binary(vector<int>& nums,int target,int l,int r)
+    {
+    if(l>r )return l;
+        int mid=l + (r-l)/2;
+        if(nums[mid]==target)
+            return mid;
+        else if(nums[mid]>target)
+            return binary(nums,target,l,mid-1);
+        else
+            return binary(nums,target,mid+1,r);
+        
+        
+    }
+};
