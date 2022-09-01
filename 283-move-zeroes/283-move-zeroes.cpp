074b31b2a690ecedb2240f6036fa39330141614f
prateek-saxena-07 <86514419+prateek-saxena-07@users.noreply.github.com>
2022-09-01 11:09:17 +0530

    Time: 31 ms (72.48%), Space: 19.1 MB (98.05%) - LeetHub

--- 283-move-zeroes/283-move-zeroes.cpp
+++ 283-move-zeroes/283-move-zeroes.cpp
@@ -1,30 +1,37 @@
 class Solution {
 public:
     void moveZeroes(vector<int>& nums) {
-        int n=nums.size();
-        int numzeroes=0;
-        for(int i=0;i< n;i++)
-        {
-         numzeroes+=(nums[i]==0) ;  
-        }
+//         int n=nums.size();
+//         int numzeroes=0;
+//         for(int i=0;i< n;i++)
+//         {
+//          numzeroes+=(nums[i]==0) ;  
+//         }
         
-        vector<int> ans;
-        for(int i=0;i<n;i++)
+//         vector<int> ans;
+//         for(int i=0;i<n;i++)
+//         {
+//             if(nums[i]!=0)
+//             {
+//                 ans.push_back(nums[i]);
+//             }
+//         }
+        
+//         while(numzeroes--)
+//         {
+//             ans.push_back(0);
+//         }
+        
+//         for(int i=0;i<n;i++)
+//         {
+//             nums[i]=ans[i];
+//         }
+        for(int lastnonzerofoundat=0,curr=0;curr<nums.size();curr++)
         {
-            if(nums[i]!=0)
+            if(nums[curr]!=0)
             {
-                ans.push_back(nums[i]);
+                swap(nums[lastnonzerofoundat++],nums[curr]);
             }
-        }
-        
-        while(numzeroes--)
-        {
-            ans.push_back(0);
-        }
-        
-        for(int i=0;i<n;i++)
-        {
-            nums[i]=ans[i];
         }
     }
 };
