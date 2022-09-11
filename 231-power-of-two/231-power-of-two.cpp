525e648eb3cc93edba7d595d3c2476183d93bdf5
prateek-saxena-07 <86514419+prateek-saxena-07@users.noreply.github.com>
2022-09-11 18:05:39 +0530

    Time: 0 ms (100.00%), Space: 5.7 MB (88.90%) - LeetHub

--- 231-power-of-two/231-power-of-two.cpp
+++ 231-power-of-two/231-power-of-two.cpp
@@ -1,7 +1,20 @@
+// class Solution {
+// public:
+//     bool isPowerOfTwo(int n) {
+//         if(n<=0) return false;
+//         return ((n&(n-1))==0);
+//     }
+// };
+
 class Solution {
 public:
     bool isPowerOfTwo(int n) {
-        if(n<=0) return false;
-        return ((n&(n-1))==0);
+        if(n<0)return false;
+        
+        int bits=__builtin_popcount(n);
+        
+        if(bits==1)
+            return true;
+        return false;
     }
 };
