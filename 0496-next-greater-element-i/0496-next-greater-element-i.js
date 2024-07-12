736c9667cce7253982011c3632cdedbfab313746
prateek-saxena-07 <86514419+prateek-saxena-07@users.noreply.github.com>
2024-07-12 15:42:52 +0530

    Time: 62 ms (60.78%), Space: 51.6 MB (67.77%) - LeetHub

---
+++ 0496-next-greater-element-i/0496-next-greater-element-i.js
@@ -0,0 +1,16 @@
+/**
+ * @param {number[]} nums1
+ * @param {number[]} nums2
+ * @return {number[]}
+ */
+var nextGreaterElement = function(nums1, nums2) {
+  const map = {};
+  const stack = [];
+  nums2.forEach(n => {
+    while (stack.length > 0 && stack[stack.length - 1] < n) {
+      map[stack.pop()] = n;
+    }
+    stack.push(n);
+  });
+  return nums1.map(n => map[n] || -1);
+};
