c2a3d45a3f1c0f29cee193e40c85c2f76e69934a
prateek-saxena-07 <86514419+prateek-saxena-07@users.noreply.github.com>
2024-07-12 15:17:15 +0530

    Time: 63 ms (47.3%), Space: 55.2 MB (6.81%) - LeetHub

--- 0042-trapping-rain-water/0042-trapping-rain-water.js
+++ 0042-trapping-rain-water/0042-trapping-rain-water.js
@@ -2,20 +2,56 @@
  * @param {number[]} height
  * @return {number}
  */
-var trap = function(height) {
-    let left_max=new Array(height.length);
-    for(let i=0;i<height.length;i++)
-    {
-        if(i=0)
-            left_max[i]=height[0];
-        else
-            left_max[i]=Math.max(height[i],left_max[i-1]);
-    }
+var trap = function(arr) {
+    
 
-    let right_max=new Array(height.length);
-    for(let i=height.length-1;i>=0;i--)
+  
+    let n=arr.length;
+    
+    let prefix=new Array(arr.length);
+    for(let i=0;i<n;i++)
     {
-        if(i==height.length-1)
-                right_max[i]=height[height.length]
+        if(i==0)
+            prefix[0]=arr[0];
+        else
+        {
+            if(arr[i]>prefix[i-1])
+                prefix[i]=arr[i];
+            else
+                prefix[i]=prefix[i-1];
+        }
     }
+   
+    let suffix=new Array(n);
+    for(let i=n-1;i>=0;i--)
+    {
+        if(i==n-1)
+            suffix[i]=arr[i];
+        else
+        {
+            if(arr[i]>suffix[i+1])
+                suffix[i]=arr[i];
+            else
+                suffix[i]=suffix[i+1];
+        }
+    }
+   
+    let minimum=new Array(n);
+    for(let i=0;i<n;i++)
+    {
+        if(suffix[i]<prefix[i])
+            minimum[i]=suffix[i];
+        else
+            minimum[i]=prefix[i];
+    }
+  
+    let water=new Array(n);
+    for(let i=0;i<n;i++)
+        water[i]=minimum[i]-arr[i];
+    
+    let sum=0;
+    for(let i=0;i<n;i++)
+        sum+=water[i];
+    return sum;
+
 };
