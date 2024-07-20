0ec26fa447264a64403495805b838463305c1d92
prateek-saxena-07 <86514419+prateek-saxena-07@users.noreply.github.com>
2024-07-20 19:37:38 +0530

    Time: 67 ms (79.21%), Space: 58.9 MB (14.66%) - LeetHub

--- 0053-maximum-subarray/0053-maximum-subarray.js
+++ 0053-maximum-subarray/0053-maximum-subarray.js
@@ -3,19 +3,18 @@
  * @return {number}
  */
 var maxSubArray = function(nums) {
-    let sum=0;
-    let MaxSum=nums[0];
+    let sum=0
+    let max= -Infinity
 
     for(let n of nums)
     {
         if(sum<0)
         {
-            sum=0;
+            sum=0
         }
+        sum+=n
+        max=Math.max(sum,max)
 
-        sum+=n;
-        MaxSum=Math.max(sum,MaxSum)
     }
-
-    return MaxSum;
+    return max;
 };
