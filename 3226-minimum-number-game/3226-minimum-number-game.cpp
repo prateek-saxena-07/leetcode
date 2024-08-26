f964281a44ead41192d8c3ed34db541154737a1a
prateek-saxena-07 <86514419+prateek-saxena-07@users.noreply.github.com>
2024-08-26 14:25:41 +0530

    Time: 9 ms (41.09%), Space: 27.5 MB (42.66%) - LeetHub

---
+++ 3226-minimum-number-game/3226-minimum-number-game.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    vector<int> numberGame(vector<int>& nums) {
+        vector<int> ans(nums.size());
+        sort(nums.begin(),nums.end());
+
+        for(int i=0;i<nums.size();i++)
+        {
+            if(i%2==0)
+            {
+                ans[i+1]=nums[i];
+            }
+            else
+            {
+                ans[i-1]=nums[i];
+            }
+        }
+        return ans;
+    }
+};
