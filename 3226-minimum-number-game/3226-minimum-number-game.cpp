200a68bfd7c31f8dcb8136a4fc90f5bae2bc3823
prateek-saxena-07 <86514419+prateek-saxena-07@users.noreply.github.com>
2024-08-26 14:38:18 +0530

    Time: 7 ms (64.97%), Space: 27.5 MB (63.4%) - LeetHub

--- 3226-minimum-number-game/3226-minimum-number-game.cpp
+++ 3226-minimum-number-game/3226-minimum-number-game.cpp
@@ -3,7 +3,7 @@ public:
     vector<int> numberGame(vector<int>& nums) {
         vector<int> ans(nums.size());
         sort(nums.begin(),nums.end());
-
+// sort(arr.begin(),arr.end());
         for(int i=0;i<nums.size();i++)
         {
             if(i%2==0)
@@ -16,5 +16,10 @@ public:
             }
         }
         return ans;
+    //      for (size_t i = 0; i < arr.size(); i += 2) {
+    //     // Use std::tie to unpack and swap adjacent elements
+    //     std::tie(arr[i], arr[i + 1]) = std::make_tuple(arr[i + 1], arr[i]);
+    // }
+    // return arr;
     }
 };
