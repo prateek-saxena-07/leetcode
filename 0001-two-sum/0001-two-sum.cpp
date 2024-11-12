7cda018bba884c75398ae96daacabf4087b3ea5b
prateek-saxena-07 <86514419+prateek-saxena-07@users.noreply.github.com>
2024-11-12 23:36:04 +0530

    Time: 88 ms (17.51%), Space: 13 MB (71.07%) - LeetHub

--- 0001-two-sum/0001-two-sum.cpp
+++ 0001-two-sum/0001-two-sum.cpp
@@ -1,19 +1,14 @@
 class Solution {
 public:
-    vector<int> twoSum(vector<int>& nums, int target) {
-     vector<int>ans;
-     unordered_map<int,int>mpp;
-
-     for(int i=0;i<nums.size();i++)
-     {
-         if(mpp.find(target-nums[i])!=mpp.end())
-         {//if the difference is found in the map return the current index and index of difference as pair 
-             ans.push_back(mpp[target-nums[i]]);
-             ans.push_back(i);
-             return ans;
-         }
-         mpp[nums[i]]=i; //otherwise store the difference as key value in map
-     }   
-     return ans;
+    vector<int> twoSum(vector<int> &nums, int target) {
+        for (int i = 0; i < nums.size(); i++) {
+            for (int j = i + 1; j < nums.size(); j++) {
+                if (nums[j] == target - nums[i]) {
+                    return {i, j};
+                }
+            }
+        }
+        // Return an empty vector if no solution is found
+        return {};
     }
 };
