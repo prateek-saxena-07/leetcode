90d23190cbdf26d3e3ef04bbb88bff24c8457d23
prateek-saxena-07 <86514419+prateek-saxena-07@users.noreply.github.com>
2024-12-07 11:54:57 +0530

    Time: 351 ms (5%), Space: 26 MB (25.97%) - LeetHub

--- 0042-trapping-rain-water/0042-trapping-rain-water.cpp
+++ 0042-trapping-rain-water/0042-trapping-rain-water.cpp
@@ -2,15 +2,18 @@ class Solution { // 4 ms, faster than 89.31%
 public:
     int trap(vector<int>& a) {
         int n = a.size();
-        vector<int> leftMax(n), rightMax(n);
+        int leftMax[n], rightMax[n];
         leftMax[0]=a[0];
+        cout<<leftMax[0]<<" ";
         for (int i = 1; i < n; ++i) 
-            leftMax[i] = max(a[i], leftMax[i-1]);
-
+            {leftMax[i] = max(a[i], leftMax[i-1]);
+            cout<<leftMax[i]<<" ";}
+cout<<endl;
             rightMax[n-1]=a[n-1];
         for (int i = n-2; i >= 0; --i) 
-            rightMax[i] = max(a[i], rightMax[i+1]);
-        
+            {rightMax[i] = max(a[i], rightMax[i+1]);
+            cout<<rightMax[i]<<" ";}
+        cout<<rightMax[n-1];
         int ans = 0;
         for (int i = 0; i < n; ++i) {
             ans=ans+(min(leftMax[i],rightMax[i])-a[i]);
