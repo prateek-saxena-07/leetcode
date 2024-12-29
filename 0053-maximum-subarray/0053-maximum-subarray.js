541d1fadab9998a0553b35cbbf485b10223b87e5
prateek-saxena-07 <86514419+prateek-saxena-07@users.noreply.github.com>
2024-12-29 10:22:45 +0530

    Time: 2 ms (57.5%), Space: 58.7 MB (23.16%) - LeetHub

--- 0053-maximum-subarray/0053-maximum-subarray.js
+++ 0053-maximum-subarray/0053-maximum-subarray.js
@@ -3,18 +3,19 @@
  * @return {number}
  */
 var maxSubArray = function(nums) {
-    let sum=0
-    let max= -Infinity
-
-    for(let n of nums)
+    let sum=0;
+    let max=-Infinity
+    for(let i=0;i<nums.length;i++)
     {
-        if(sum<0)
-        {
-            sum=0
-        }
-        sum+=n
+        sum=sum+nums[i];
+
+        //max check
         max=Math.max(sum,max)
 
+        if(sum<0)
+        {
+            sum=0;
+        }
     }
     return max;
 };
